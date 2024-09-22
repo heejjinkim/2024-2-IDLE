89b2ed1e4eb154e1bf20185da64cfcb35154d862
likerhythm <wjdalsdl0412@gmail.com>
2024-09-22 21:09:34 +0900

    init commit

---
+++ src/test/java/com/example/__2_IDLE/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__2_IDLE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
