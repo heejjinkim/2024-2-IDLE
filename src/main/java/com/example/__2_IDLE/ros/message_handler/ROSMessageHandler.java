78f01e559266de9855990649b070edc6712a73ab
likerhythm <wjdalsdl0412@gmail.com>
2024-10-01 12:18:30 +0900

    [FEAT] TopicRobotPoseMessageHandler 구현

---
+++ src/main/java/com/example/__2_IDLE/ros/message_handler/ROSMessageHandler.java
@@ -0,0 +1,15 @@
+package com.example.__2_IDLE.ros.message_handler;
+
+import com.example.__2_IDLE.ros.message_value.MessageValue;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public abstract class ROSMessageHandler<T extends MessageValue> {
+    String messageName;
+    String messageType;
+
+    abstract public T processMessage(String message);
+    abstract protected T parsingMessage(String message);
+}
---
+++ src/main/java/com/example/__2_IDLE/ros/message_handler/TopicRobotPoseMessageHandler.java
@@ -0,0 +1,56 @@
+package com.example.__2_IDLE.ros.message_handler;
+
+import com.example.__2_IDLE.ros.message_value.RobotPoseMessageValue;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@Slf4j
+public class TopicRobotPoseMessageHandler extends ROSMessageHandler<RobotPoseMessageValue>{
+
+    String namespace;
+
+    public TopicRobotPoseMessageHandler(String namespace) {
+        this.messageName = "/tf";
+        this.messageType = "tf2_msgs/TFMessage";
+        this.namespace = namespace;
+    }
+    @Override
+    public RobotPoseMessageValue processMessage(String message) {
+        return parsingMessage(message);
+    }
+
+    @Override
+    protected RobotPoseMessageValue parsingMessage(String message) {
+        RobotPoseMessageValue messageValue = new RobotPoseMessageValue();
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(message);
+            JsonNode transformsNode = rootNode.path("transforms").get(0);
+            String frameId = transformsNode.path("header").path("frame_id").asText();
+
+            if (frameId.equals("odom")) {
+                log.info("frameId: {}", frameId);
+                JsonNode translationNode = transformsNode
+                        .path("transform")
+                        .path("translation");
+                double x = translationNode.path("x").asDouble();
+                double y = translationNode.path("y").asDouble();
+                ((RobotPoseMessageValue) messageValue).setX(x);
+                ((RobotPoseMessageValue) messageValue).setY(y);
+                return messageValue;
+            } else {
+                log.warn("Received message with different frame_id: {}", frameId);
+                return messageValue;  // 빈 값을 반환하거나 예외를 던질 수 있음
+            }
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
