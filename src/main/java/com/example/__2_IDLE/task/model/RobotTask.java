7175286b6fd7d75a990c11a6655fdf20b9ce35d4
heejjinkim <06.hjhj.12@gmail.com>
2024-10-03 13:52:05 +0900

    [FIX] RobotTask 선반 원위치 목적지 추가

--- src/main/java/com/example/__2_IDLE/task/model/RobotTask.java
+++ src/main/java/com/example/__2_IDLE/task/model/RobotTask.java
@@ -4,14 +4,10 @@ import com.example.__2_IDLE.global.model.enums.Item;
 import com.example.__2_IDLE.global.model.enums.Station;
 import com.example.__2_IDLE.robot_manager.pos.Pos;
 import java.util.LinkedList;
-import java.util.Queue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -19,17 +15,18 @@ import java.util.List;
 @Builder
 public class RobotTask {
 
-  private Long id;
-  private LinkedList<Pos> destinations;
+    private Long id;
+    private LinkedList<Pos> destinations;
 
-  public static RobotTask of(Item item, LinkedList<Station> stations) {
-    LinkedList<Pos> posList = new LinkedList<>();
-    posList.add(item.getShelf().getPos());
-    for (Station station : stations) {
-      posList.add(station.getPos());
+    public static RobotTask of(Item item, LinkedList<Station> stations) {
+        LinkedList<Pos> posList = new LinkedList<>();
+        posList.add(item.getShelf().getPos());
+        for (Station station : stations) {
+            posList.add(station.getPos());
+        }
+        posList.add(item.getShelf().getPos());
+        return RobotTask.builder()
+            .destinations(posList)
+            .build();
     }
-    return RobotTask.builder()
-        .destinations(posList)
-        .build();
-  }
 }
