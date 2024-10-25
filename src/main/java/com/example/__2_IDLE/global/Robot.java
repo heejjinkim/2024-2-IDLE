0927b6ce0d540235ce6242f6d3557c4b0c9097d5
likerhythm <wjdalsdl0412@gmail.com>
2024-10-25 18:20:49 +0900

    refactor: Robot 클래스 위치 global로 변경

--- src/main/java/com/example/__2_IDLE/global/Robot.java
+++ src/main/java/com/example/__2_IDLE/global/Robot.java
@@ -1,9 +1,7 @@
-package com.example.__2_IDLE.robot_manager.robot;
+package com.example.__2_IDLE.global;
 
 import com.example.__2_IDLE.global.model.enums.Shelf;
 import com.example.__2_IDLE.global.model.Pose;
-import com.example.__2_IDLE.robot_manager.state.RobotState;
-import com.example.__2_IDLE.robot_manager.state.RobotStateContainer;
 import com.example.__2_IDLE.task.model.RobotTask;
 import java.util.LinkedList;
 import lombok.Getter;
@@ -18,32 +16,16 @@ import lombok.ToString;
 public class Robot {
 
     private String namespace;
-    private Pose pose;             // 현재 위치
-    private RobotState state;    // 현재 상태
+    private Pose currentPose;           // 현재 위치
     private Shelf shelf;         // 현재 운반 중인 선반
     private RobotTask robotTask; // 현재 할당 받은 작업
     private LinkedList<RobotTask> taskQueue; // 로봇의 작업 큐
 
     public Robot(String namespace, Pose pose) {
         this.namespace = namespace;
-        this.pose = pose;
-        this.state = RobotStateContainer.getWaitStateInstance();
+        this.currentPose = pose;
         this.shelf = null;
         this.robotTask = null;
         this.taskQueue = new LinkedList<>();
-    }
-
-    public void setRobotTask(RobotTask robotTask) {
-        this.robotTask = robotTask;
-        this.state = RobotStateContainer.getCarryStateInstance();
-    }
-
-    public void startTask() { // 로봇의 작업 수행
-        state.startTask(this);
-    }
-
-    public void resetRobotTask() {
-        this.robotTask = null;
-        this.shelf = null;
     }
 }
