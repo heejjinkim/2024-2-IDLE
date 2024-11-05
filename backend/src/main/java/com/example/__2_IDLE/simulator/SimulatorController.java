83b4a05ea37728ca622c6578b64e74e06b374c4e
heejjinkim <06.hjhj.12@gmail.com>
2024-11-06 02:31:47 +0900

    [FIX] api 오류 수정

--- backend/src/main/java/com/example/__2_IDLE/simulator/SimulatorController.java
+++ backend/src/main/java/com/example/__2_IDLE/simulator/SimulatorController.java
@@ -2,10 +2,12 @@ package com.example.__2_IDLE.simulator;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/simulator")
+@RestController
+@RequestMapping("/simulator")
 public class SimulatorController {
     private final SimulatorService simulatorService;
 
