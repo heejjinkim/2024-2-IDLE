dc88c7fa4f94dc16f16daa3f7ff03cd89af319e1
heejjinkim <06.hjhj.12@gmail.com>
2024-11-11 23:01:18 +0900

    [FIX] orders 초기화

--- backend/src/main/java/com/example/__2_IDLE/order/OrderRepository.java
+++ backend/src/main/java/com/example/__2_IDLE/order/OrderRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public class OrderRepository {
 
-    private List<Order> orders;
+    private List<Order> orders = new ArrayList<>();
 
     public void save(Order order) {
         orders.add(order);
